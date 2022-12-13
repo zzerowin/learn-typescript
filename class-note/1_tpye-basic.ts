1058b866ef224a0a9bad08cc56525fd5dc54c694
zzerowin <alaykr123@gmail.com>
2022-12-13 17:18:09 +0900

    기본 타입 - 튜플, 객체, 진위값

--- class-note/1_tpye-basic.ts
+++ class-note/1_tpye-basic.ts
@@ -11,3 +11,21 @@ const num: number = 20;
 const arr: Array<number> = [1, 2, 3];
 const heroes: Array<string> = ['Capt', "Thor", 'Hulk'];
 const items: number[] = [1, 2, 3, 4];
+
+// TS 튜플
+const address: [string, number] = ['gangnam', 123];
+
+// TS 객체
+const obj: object = {};
+// const person: object = {
+//   name: 'zero',
+//   age: 10,
+// }
+
+const person: {name: string, age: number} = {
+  name: 'zero',
+  age: 12,
+}
+
+// TS 진위값
+const show: boolean = true;
